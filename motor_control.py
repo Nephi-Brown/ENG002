c4cfed8188f309a8a8196f1611cd2ca7a742e653
Annabel2005 <wnt521@york.ac.uk>
2025-11-10 14:28:34 +0000

    Add files via upload

--- motor_control.py
+++ motor_control.py
@@ -9,36 +9,36 @@ MR1_PinA=Pin(17,Pin.OUT) #backward pin
 pwm1=PWM(Pin(19))
 pwm1.freq(1000)#sets the pwm for 1000 Hz(turns it on and off 1000 times a second)
 
-#Motor 2
+#Motor 2 (pins need to be changed)
 MR2_PinB=Pin(18,Pin.OUT) #forward pin
 MR2_PinA=Pin(17,Pin.OUT) #backward pin
 pwm2=PWM(Pin(19))
 pwm2.freq(1000)#sets the pwm for 1000 Hz(turns it on and off 1000 times a second)
 
-def move_robot(dir1, dir2, speed): # function takes directions for motors and there speed
+def move_robot(dir1, dir2, speed): # function takes directions for motors and their speed
     # control for motor 1
     if dir1 == "forward":
         MR1_PinA.value(0)
         MR1_PinB.value(1)
     elif dir1 == "backward":
-        MR1_PinA.value(0)
-        MR1_PinB.value(1)
+        MR1_PinA.value(1)
+        MR1_PinB.value(0)
     else:
         MR1_PinA.value(0)
         MR1_PinB.value(0)
-    pwm1.duty_u16(speed*65535)
+    pwm1.duty_u16(int(speed*65535))
     
     #control for motor 2  
     if dir2 == "forward":
         MR2_PinA.value(0)
         MR2_PinB.value(1)
     elif dir2 == "backward":
-        MR2_PinA.value(0)
-        MR2_PinB.value(1)
+        MR2_PinA.value(1)
+        MR2_PinB.value(0)
     else:
         MR2_PinA.value(0)
         MR2_PinB.value(0)
-    pwm2.duty_u16(speed*65535)  
+    pwm2.duty_u16(int(speed*65535))  
  
 
 while True:
