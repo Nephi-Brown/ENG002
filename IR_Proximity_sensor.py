228bf1d197b7d284ecd5a5d37406896f96813068
Annabel2005 <wnt521@york.ac.uk>
2025-11-14 14:07:11 +0000

    Add files via upload

--- IR_Proximity_sensor.py
+++ IR_Proximity_sensor.py
@@ -1,14 +1,14 @@
 from machine import Pin
-
+from time import sleep
 
 sensor = Pin(15, Pin.IN)  #set GPIO-25 pin mode as INPUT
 
 while True:
     if sensor.value() == 0:  #read the value of the sensor pin and compare it with 0
-        print "object detected"
+        print("object detected")
     else:
-        print "no object"
+        print("no object")
  
-    
+    sleep(0.5)
    
  
